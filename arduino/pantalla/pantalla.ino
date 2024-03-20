a265dcf2dc5e26c274bc3e86ce6f55c6ae19c65a
Diego <diegosanchez1234567@gmail.com>
2024-03-20 10:37:23 -0300

    arduino

--- arduino/pantalla/pantalla.ino
+++ arduino/pantalla/pantalla.ino
@@ -22,13 +22,13 @@ esta configuracion de estos pines se encuentran dentro de la librería "wire" ma
 
 #include <LiquidCrystal_I2C.h> //DESCARGAR LIBRERÍA:https://github.com/ELECTROALL/Codigos-arduino/blob/master/LiquidCrystal_I2C.zip
 
-LiquidCrystal_I2C lcd(0x3f,16,2); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
+LiquidCrystal_I2C lcd(0x27,16,2); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
 
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
- lcd.setCursor(0,0);
+  lcd.setCursor(0,0);
   lcd.print("==> VIDEOS <=="); 
   lcd.setCursor (0,1);
   lcd.print("==> ELECTROALL <");
